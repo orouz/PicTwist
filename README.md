9ce649ba5bb25e4a3a94f781a1bfef383f6bd708
orouz <oro@welldone-software.com>
2017-04-16 01:27:51 +0300

    dynamic options

--- README.md
+++ README.md
@@ -10,10 +10,14 @@ import Pictwist from 'pictwist'
 /* get img elements to play with */
 const imgs = Array.from(document.querySelectorAll('img'))
 
-/* set a transform function for each color property */
+/* 
+set a transform function for each color property
+that returns an integer between 0-256 
+you get the current value as the only argument of the function 
+ */
 const options = {
         red: x => 256  // will set pixels to red  
-        blue: x => Math.random() * 256, // will set some pixels to blue 
+        blue: x => Math.random() * 256, // will set pixels blueish 
         green: x => x // default, does nothing (optional) 
         alpah: x => x  // same 
 }
--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-export default  class Twist {
-    constructor(el, options = {}) {
+export default class Twist {
+  constructor(el, options = {}) {
 
-        if (!el instanceof Element || el.tagName !== 'IMG')
-            throw new Error(`PicTwist expects an <img> element, can't work like this man. come on..`)
+    if (!el instanceof Element || el.tagName !== 'IMG') 
+      throw new Error(`PicTwist expects an <img> element, can't work like this man. come on..`)
 
+    this.el = el
+    const defaults = ['red', 'green', 'blue', 'alpah'].reduce((a, c) => { a[c] = x => x; return a}, {})
+    this.setOptions(options, defaults)
+    this.setCanvasPosition()
+    this.loadImage()
+  }
+  get height() {
+    return this.el.height
+  }
+  get width() {
+    return this.el.width
+  }
+  setOptions(options, defaults) {
 
-        const defaults = ['red','green','blue','alpah'].reduce((a,c) =>{a[c] = x => x; return a} ,{})
-        this.options = Object.assign({}, defaults, options)
-        this.el = el
-        this.setCanvasPosition()
-        this.loadImage()
-    }
-    get height(){
-      return this.el.height 
-    }
-    get width(){
-      return this.el.width 
-    }
-    setCanvasPosition() {
-     
-        const canvas = document.createElement('canvas')
-        const props = this.el.getClientRects()[0]
-        this.ctx = canvas.getContext('2d')
-        this.canvas = canvas
-        
-        canvas.width = this.width
-        canvas.height = this.height
-        canvas.style.position = 'absolute'
-        canvas.style.left = props.left + 'px'
-        canvas.style.top = props.top + 'px'
+    this.options = Object.assign({}, defaults, options)
+  }
+  setCanvasPosition() {
 
-        document.body.appendChild(canvas)
-    }
+    const canvas = document.createElement('canvas')
+    const props = this.el.getClientRects()[0]
+    this.ctx = canvas.getContext('2d')
+    this.canvas = canvas
 
-    loadImage() {
-        const img = new Image(this.width,this.height)
-        img.crossOrigin = "Anonymous";
-        img.src = this.el.getAttribute('src')
-        img.onload = this.draw.bind(this)
-        this.img = img
-    }
-    draw() {
+    canvas.width = this.width
+    canvas.height = this.height
+    canvas.style.position = 'absolute'
+    canvas.style.left = props.left + 'px'
+    canvas.style.top = props.top + 'px'
 
-        const {ctx,img,options} = this
-        ctx.drawImage(img, 0, 0,this.width,this.height);
-   
-        const imgData = ctx.getImageData(0, 0, this.width, this.height)
-        const data = imgData.data
-        for (var i = 0; i < data.length; i += 4) {
-            
-            data[i]     = options.red(data[i])
-            data[i + 1] = options.green(data[i + 1])
-            data[i + 2] = options.blue(data[i + 2])
-            data[i + 3] = options.alpah(data[i + 3])
-        }
-        ctx.putImageData(imgData, 0, 0);
+    document.body.appendChild(canvas)
+  }
+
+  loadImage() {
+    const img = new Image(this.width, this.height)
+    img.crossOrigin = "Anonymous";
+    img.src = this.el.getAttribute('src')
+    img.onload = this.drawImage.bind(this)
+    this.img = img
+  }
+  drawImage() {
+
+    this.ctx.drawImage(this.img, 0, 0, this.width, this.height);
+    this.imgData = this.ctx.getImageData(0, 0, this.width, this.height)
+    this.setColors()
+  }
+
+  setColors() {
+    const {ctx, options} = this
+    const data = this.imgData.data
+    for (var i = 0; i < data.length; i += 4) {
+
+      data[i] = options.red(data[i])
+      data[i + 1] = options.green(data[i + 1])
+      data[i + 2] = options.blue(data[i + 2])
+      data[i + 3] = options.alpah(data[i + 3])
+
     }
+    ctx.putImageData(this.imgData, 0, 0);
+  }
+  updateOptions(options) {
+    this.setOptions(options, this.options)
+    this.setColors()
+  }
+  destroy() {
+    document.body.removeChild(this.canvas)
+  }
 }
-
-
-
-
